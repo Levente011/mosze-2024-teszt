1a64ef0ca9449726710bb360136876b317b88769
Szebik <szebik.levente@gmail.com>
2024-09-05 14:12:49 +0200

    Hibák kommentelése.

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];  //NELEMENTS helyett N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa'  // '' helyett "", ; hiánya
+    for (int i = 0;)  // Hibás for ciklus
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)  // Rossz ciklus
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:";  // Nincs kiírva a tényleges érték
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;  // a változónak értéket adni
+    for (int i = 0; i < N_ELEMENTS, i++)  // , helyett ;
     {
-        atlag += b[i]
+        atlag += b[i] // ; hiánya a sor végén
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
